0393585b20f54732037c1b3badd1542f18b1a5f1
eranetsa <41900050+eranetsa@users.noreply.github.com>
2026-02-22 17:12:05 +0300

    ..

--- era_spy_recruitment/__manifest__.py
+++ era_spy_recruitment/__manifest__.py
@@ -17,7 +17,6 @@
     "data": [
         "security/ir.model.access.csv",
         "data/ir_cron.xml",
-        "data/ir_config_parameter.xml",
         "views/res_config_settings_views.xml",
         "views/hr_applicant_views.xml",
         "views/eraspy_applicant_callback_queue_views.xml",

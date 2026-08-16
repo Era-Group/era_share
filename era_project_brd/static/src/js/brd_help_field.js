f4c37de4ad20bd653dfc4e62e0efdde2cff5ba3c
Odoo User <user@example.com>
2026-08-16 01:29:28 +0000

    era_project_brd: make workflow help scrollable

--- era_project_brd/static/src/js/brd_help_field.js
+++ era_project_brd/static/src/js/brd_help_field.js
@@ -1,13 +1,29 @@
 /** @odoo-module **/
 
 import { _t } from "@web/core/l10n/translation";
+import { Dialog } from "@web/core/dialog/dialog";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { Component } from "@odoo/owl";
+
+export class BrdWorkflowHelpDialog extends Component {
+    static template = "era_project_brd.BrdWorkflowHelpDialog";
+    static components = { Dialog };
+    static props = { close: Function };
+}
 
 export class BrdWorkflowHelpField extends Component {
     static template = "era_project_brd.BrdWorkflowHelpField";
     static props = { ...standardFieldProps };
+
+    setup() {
+        this.dialog = useService("dialog");
+    }
+
+    openHelp() {
+        this.dialog.add(BrdWorkflowHelpDialog);
+    }
 }
 
 export const brdWorkflowHelpField = {
